cc2b7f37247c97e1785d92e820eb3b1d336120d8
Asura336 <w41312@outlook.com>
2025-11-04 16:20:24 +0800

    Update StageCursorsRegister.cs
    改为手动调用，减少插件的侵入性

--- FairyGUI_ForUnity3D_Project/Packages/com.unity.fairygui/Runtime/Extensions/FGUICursorConfig/StageCursorsRegister.cs
+++ FairyGUI_ForUnity3D_Project/Packages/com.unity.fairygui/Runtime/Extensions/FGUICursorConfig/StageCursorsRegister.cs
@@ -1,22 +1,26 @@
-using System;
+﻿using System;
 using System.Linq;
 using UnityEngine;
 
 namespace FairyGUI.Extensions
 {
-    static class StageCursorsRegister
+    public static class StageCursorsRegister
     {
         const string ROOT = "FGUICursors/";
         const string intro = "cursordefs";
 
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-        static void RegistCursors()
+        /// <summary>
+        /// 找到脚本同级的 Resources 文件夹，里面是常用光标图片和中心点配置
+        /// </summary>
+        public static void RegistCursors()
         {
             var introFile = Resources.Load<TextAsset>(ROOT + intro);
             var stage = Stage.inst;
-            foreach (var line in introFile.text.Split('\r', '\n').Where(c => !string.IsNullOrEmpty(c)))
+            foreach (var line in introFile.text.Split('\r', '\n')
+                .Where(c => !string.IsNullOrEmpty(c)))
             {
-                var group = line.Split(',');
+                // {cursor name},{png file name},{hotspot x},{hotspot y}
+                var group = line.Split(',', 4);
                 var name = group[0];
                 var texture = Resources.Load<Texture2D>(ROOT + group[1]);
                 var hotspot = new Vector2(Convert.ToSingle(group[2]), Convert.ToSingle(group[3]));
